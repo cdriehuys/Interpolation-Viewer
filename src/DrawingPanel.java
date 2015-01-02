a04d726b840651f3bf322ff57c55f2e2d1d62912
Chathan <cdriehuys@gmail.com>
2015-01-02 14:01:24 -0500

    Implementing linear interpolation and a gui to show it

--- src/DrawingPanel.java
+++ src/DrawingPanel.java
@@ -1,14 +1,25 @@
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
-public class DrawingPanel extends JPanel implements ActionListener {
+public class DrawingPanel extends JPanel implements ActionListener, ChangeListener {
+
+    private static int T_MIN = 0;
+    private static int T_MAX = 1000;
+    private static int T_INIT = 0;
+    private static int T_INFLATION = 1000;
+
+    private ArrayList<Vector2> points;
 
     private int width, height;
 
-    private ArrayList<Vector2> points;
+    private JLabel tLabel;
+
+    private JSlider tSlider;
 
     private Timer repaintTimer;
 
@@ -20,23 +31,48 @@ public class DrawingPanel extends JPanel implements ActionListener {
         setPreferredSize(new Dimension(width, height));
 
         this.points = new ArrayList<Vector2>();
-        this.points.add(new Vector2(25, 10));
-        this.points.add(new Vector2(30, 25));
+        this.points.add(new Vector2(50, 100));
+        this.points.add(new Vector2(400, 375));
 
         this.repaintTimer = new Timer(20, this);
+
+        JPanel slidePanel = new JPanel();
+
+        this.tSlider = new JSlider(JSlider.HORIZONTAL, T_MIN, T_MAX, T_INIT);
+        this.tSlider.addChangeListener(this);
+        slidePanel.add(this.tSlider);
+
+        this.tLabel = new JLabel(String.format("t = %.3f", (float)this.tSlider.getValue() / T_INFLATION));
+        slidePanel.add(this.tLabel);
+
+        add(slidePanel);
     }
 
     @Override
     public void paint(Graphics g) {
         super.paint(g);
 
+        drawLine(g, this.points.get(0), this.points.get(1));
+
         for (Vector2 p : this.points) {
             p.draw(g);
         }
+
+        Vector2.lerp(this.points.get(0), this.points.get(1), (float)this.tSlider.getValue() / T_INFLATION).draw(g);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         repaint();
+    }
+
+    @Override
+    public void stateChanged(ChangeEvent e) {
+        this.tLabel.setText(String.format("t = %.2f", (float)this.tSlider.getValue() / T_INFLATION));
+        repaint();
+    }
+
+    public void drawLine(Graphics g, Vector2 start, Vector2 end) {
+        g.drawLine(Math.round(start.getX()), Math.round(start.getY()), Math.round(end.getX()), Math.round(end.getY()));
     }
 }
--- src/Vector2.java
+++ src/Vector2.java
@@ -5,9 +5,9 @@ public class Vector2 {
     public static int DEFAULT_RADIUS = 8;
 
 
-    private int x, y;
+    private float x, y;
 
-    public Vector2(int x, int y) {
+    public Vector2(float x, float y) {
 
         this.x = x;
         this.y = y;
@@ -15,6 +15,24 @@ public class Vector2 {
 
     public void draw(Graphics g) {
 
-        g.fillOval(this.x, this.y, DEFAULT_RADIUS, DEFAULT_RADIUS);
+        g.fillOval(Math.round(this.x), Math.round(this.y), DEFAULT_RADIUS, DEFAULT_RADIUS);
     }
+
+    public static Vector2 lerp(Vector2 start, Vector2 end, float t) {
+
+        t = (t < 0) ? 0 : t;
+        t = (t > 1) ? 1 : t;
+
+        return new Vector2((1 - t) * start.x + t * end.x, (1 - t) * start.y + t * end.y);
+    }
+
+    /*
+    GETTER AND SETTER METHODS
+     */
+
+    public float getX() { return this.x; }
+    public void setX(int x) { this.x = x; }
+
+    public float getY() { return this.y; }
+    public void setY(int y) { this.y = y; }
 }
