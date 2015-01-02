b571af0356eafe38b0ee4934ed92072d8ffbcd07
Chathan <cdriehuys@gmail.com>
2015-01-02 12:27:12 -0500

    Initial commitwith basic JFrame

---
+++ src/InterpolationViewer.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+
+public class InterpolationViewer extends JFrame {
+
+    public static final int WIN_WIDTH = 500;
+    public static final int WIN_HEIGHT = 500;
+
+    public InterpolationViewer() {
+
+        setSize(WIN_WIDTH, WIN_HEIGHT);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(new Runnable() {
+
+            @Override
+            public void run() {
+                new InterpolationViewer();
+            }
+        });
+    }
+}
